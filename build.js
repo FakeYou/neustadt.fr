cd548f8bca1bb4b1a77f067ad1fd6abdbfe1dbeb
Andre Nanninga <ag.nanninga@gmail.com>
2018-06-09 09:51:46 +0200

    clean-up

--- build.js
+++ build.js
@@ -1,27 +1,27 @@
-var metalsmith        = require('metalsmith'),
-    markdown          = require('metalsmith-markdown'),
-    drafts            = require('metalsmith-drafts'),
-    layouts           = require('metalsmith-layouts'),
-    permalinks        = require('metalsmith-permalinks'),
-    collections       = require('metalsmith-collections'),
-    handlebars        = require('handlebars'),
-    metallic          = require('metalsmith-metallic'),
-    serve             = require('metalsmith-serve'),
-    watch             = require('metalsmith-watch'),
-    redirect		  = require('metalsmith-redirect');
+const metalsmith        = require('metalsmith');
+const markdown          = require('metalsmith-markdown');
+const drafts            = require('metalsmith-drafts');
+const layouts           = require('metalsmith-layouts');
+const permalinks        = require('metalsmith-permalinks');
+const collections       = require('metalsmith-collections');
+const handlebars        = require('handlebars');
+const metallic          = require('metalsmith-metallic');
+const serve             = require('metalsmith-serve');
+const watch             = require('metalsmith-watch');
+const redirect          = require('metalsmith-redirect');
 
 handlebars.registerHelper('moment', require('helper-moment'));
 
 // limit an array to a maximum of elements (from the start)
 // Thanks to http://stackoverflow.com/questions/10377700/limit-results-of-each-in-handlebars-js
 handlebars.registerHelper('each_upto', function(ary, max, options) {
-    if(!ary || ary.length == 0)
-        return options.inverse(this);
+  if(!ary || ary.length == 0)
+    return options.inverse(this);
 
-    var result = [ ];
-    for(var i = 0; i < max && i < ary.length; ++i)
-        result.push(options.fn(ary[i]));
-    return result.join('');
+  var result = [ ];
+  for(var i = 0; i < max && i < ary.length; ++i)
+    result.push(options.fn(ary[i]));
+  return result.join('');
 });
 
 metalsmith(__dirname)
@@ -31,7 +31,7 @@ metalsmith(__dirname)
       baseurl: 'https://www.neustadt.fr',
       author: 'Parimal Satyal',
       keywords: 'Parimal Satyal, Neustadt, parimalsatyal',
-			description: 'Personal website of Parimal Satyal, a designer/geek who enjoys cosmology, dark beer, the open internet, metal music and foreign languages. Essays, music and reviews.'
+      description: 'Personal website of Parimal Satyal, a designer/geek who enjoys cosmology, dark beer, the open internet, metal music and foreign languages. Essays, music and reviews.'
     }
   })
   .source('./src')
@@ -74,28 +74,28 @@ metalsmith(__dirname)
     pattern: ["*/*/*html","*/*html","*html"],
     default: 'essay.html',
     partials: {
-            header: 'partials/header',
-            footer: 'partials/footer'
-        }
+      header: 'partials/header',
+      footer: 'partials/footer'
+    }
   }))
-  .use(serve({
-    port: 8081,
-    verbose: true
+  // .use(serve({
+  //   port: 8081,
+  //   verbose: true
+  // }))
+  // .use(watch({
+  //   paths: {
+  //     "${source}/**/*": true,
+  //     "layout/**/*": "**/*",
+  //   }
+  // }))
+  .use(redirect({
+    '/about': '/parimal-satyal',
   }))
-  .use(watch({
-      paths: {
-        "${source}/**/*": true,
-        "layout/**/*": "**/*",
-      }
-    }))
-		.use(redirect({
-		  '/about': '/parimal-satyal',
-		}))
-    .build(function (err) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log('Neustadt.fr built!');
-      }
-    });
+  .build(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Neustadt.fr built!');
+    }
+  });
